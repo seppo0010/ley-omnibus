65bca96ec88abd93fc5d62ff812997bd02aa4f77
Sebastian Waisbrot <seppo0010@gmail.com>
2024-01-02 12:00:08 -0300

    Fix link

--- src/pages/proyecto.js
+++ src/pages/proyecto.js
@@ -14,9 +14,12 @@ const Proyecto = () => {
             <div className="mt-10 md:mt-20 flex flex-col w-10/12 md:w-2/3 text-justify mx-auto my-auto h-auto font-sans">
 
                 <p className="mb-1">Adaptación de <a
+                    href={"https://resistencia.uy/"}
+                    target="_blank"
+                    rel="noopener noreferrer">LOC Comparada</a> a la ley Ómnibus. <a
                     href={"https://seppo0010.github.io/ley-omnibus/"}
                     target="_blank"
-                    rel="noopener noreferrer">LOC Comparada</a> a la ley Ómnibus.</p>
+                    rel="noopener noreferrer">Ver código fuente</a>.</p>
 
             </div>
 
